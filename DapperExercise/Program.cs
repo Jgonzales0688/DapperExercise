e0561108943ef2e09be12967089a3dad7821537f
Jessica Gonzales <jgonzales0688@gmail.com>
2023-01-05 19:35:23 -0600

    Dapper Exercise 1

--- DapperExercise/Program.cs
+++ DapperExercise/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperExercise;
+using System.Transactions;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DapperDepartmentRepository(conn);
+var departments = repo.GetAllDepartments();
+
+foreach (var item in departments)
+{
+    Console.WriteLine($"Department: {item.Name}");
+}
+
+Console.WriteLine("Type a new Department name");
+var newDepartment = Console.ReadLine();
+
+repo.InsertDeparment(newDepartment);
+IEnumerable<Department> updatedDepartments = repo.GetAllDepartments();
+
+foreach (var item in updatedDepartments)
+{
+    Console.WriteLine($"Department: {item.Name}");
+}
